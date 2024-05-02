99dfc3ab895d04c6f4aabedb26883222f0992d1b
unknown <remidescamps7@gmail.com>
2024-05-02 10:11:40 +0200

    Commit final

--- serveur.js
+++ serveur.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const cors = require ("cors")
 const swaggerDocument = require('./swagger/swagger.json'); // Assure-toi que le chemin est correct
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ const router = require('./routes/routes');
 // Utilisation du middleware pour parser les requêtes JSON et les données encodées dans l'URL
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors()); //utilisation du middleware cors
 
 // Utilisation du routeur pour gérer les différentes routes de l'API
 app.use(`/api/${version}/`, router);
